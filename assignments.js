376036266837636c4f747efe849e55c7cd533593
AlukardYahvi <yahvi.edsel.hernandez@gmail.com>
2025-11-27 15:37:07 -0600

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,16 @@
+const ASSIGNMENTS = {
+  "araceli.hernandez": ["II-008-PL-MC","II-039-PL-MC"],
+  "marycarmen.godinez": ["II-009-PL-MC","II-043-PL-NT","II-047-PP-NT","II-088-PP-MC"],
+  "claudia.calzada": ["II-010-PL-NT","II-045-PL-NT","II-049-PP-ET","II-092-PP-GE"],
+  "paola.zarate": ["II-012-PL-MC","II-048-PL-MC","II-068-PL-PG","II-004-PP-NT","II-053-PP-GE","II-093-PP-NT"],
+  "ivonne.maciel": ["II-016-PL-NT","II-050-PL-NT","II-075-PL-NT","II-006-PP-GE","II-055-PP-MC","II-097-PP-ET"],
+  "aldo.resendiz": ["II-020-PL-NT","II-051-PL-GE","II-076-PL-NT","II-007-PP-NT","II-057-PP-NT"],
+  "javier.perez": ["II-022-PL-NT","II-054-PL-NT","II-086-PL-ET","II-011-PP-PG","II-061-PP-NT"],
+  "paula.saldana": ["II-024-PL-NT","II-058-PL-NT","II-089-PL-ET","II-013-PP-GE","II-069-PP-NT"],
+  "elena.cruz": ["II-025-PL-NT","II-059-PL-NT","II-070-PP-NT","II-091-PL-ET"],
+  "nury.perez": ["II-027-PL-MC","II-060-PL-GE","II-094-PL-NT","II-015-PP-PG","II-072-PP-NT"],
+  "enrique.mendez": ["II-029-PL-ET","II-062-PL-NT","II-095-PL-ET","II-019-PP-ET","II-081-PP-NT"],
+  "edgar.cano": ["II-032-PL-NT","II-064-PL-NT","II-096-PL-ET","II-023-PP-GE","II-082-PP-NT"],
+  "javier.flores": ["II-033-PL-ET","II-066-PL-NT","II-098-PL-NT","II-036-PP-NT","II-083-PP-NT"],
+  "araceli.montoya": ["II-001-PP-NT","II-044-PP-ET","II-085-PP-NT"]
+};

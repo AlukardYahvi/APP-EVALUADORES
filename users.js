c4cd1662bd6dd4c290f857a1631ef1099d57dec1
AlukardYahvi <yahvi.edsel.hernandez@gmail.com>
2025-11-27 15:36:54 -0600

    Add files via upload

---
+++ users.js
@@ -0,0 +1,18 @@
+const USERS = [
+  { user: "admin", pass: "Admin2024", name: "Administrador General", role: "admin" },
+
+  { user: "araceli.hernandez", pass: "Simposio2024", name: "Araceli Hernández Zavala", role: "juez" },
+  { user: "marycarmen.godinez", pass: "Simposio2024", name: "Marycarmen Godinez Victoria", role: "juez" },
+  { user: "claudia.calzada", pass: "Simposio2024", name: "Claudia Camelia Calzada Mendoza", role: "juez" },
+  { user: "paola.zarate", pass: "Simposio2024", name: "Paola Berenice Zarate Segura", role: "juez" },
+  { user: "ivonne.maciel", pass: "Simposio2024", name: "Ivonne Maciel Arciniega Martínez", role: "juez" },
+  { user: "aldo.resendiz", pass: "Simposio2024", name: "Aldo Arturo Reséndiz Albor", role: "juez" },
+  { user: "javier.perez", pass: "Simposio2024", name: "Javier Pérez Durán", role: "juez" },
+  { user: "paula.saldana", pass: "Simposio2024", name: "Paula Saldaña", role: "juez" },
+  { user: "elena.cruz", pass: "Simposio2024", name: "Elena de La Cruz Herrera Cogco", role: "juez" },
+  { user: "nury.perez", pass: "Simposio2024", name: "Nury Pérez Hernández", role: "juez" },
+  { user: "enrique.mendez", pass: "Simposio2024", name: "Enrique Méndez-Bolaina", role: "juez" },
+  { user: "edgar.cano", pass: "Simposio2024", name: "Edgar Cano Europa", role: "juez" },
+  { user: "javier.flores", pass: "Simposio2024", name: "Javier Flores Estrada", role: "juez" },
+  { user: "araceli.montoya", pass: "Simposio2024", name: "Araceli Montoya Estrada", role: "juez" }
+];
